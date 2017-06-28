2be15af9a512ce65839369e8bee327d1b3199a47
Tim Shi <tianlins@stanford.edu>
2017-06-28 00:37:05 -0700

    Revert "WIP: web-demo"

--- web-server/src/pages/model-view/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {Route} from "react-router-dom";
-export const ModelViews = [
-    <Route exact path="/models" key="/models" component={() => (<div>model page</div>)}/>
-];
-
-export default ModelViews;
-
-
