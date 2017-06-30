9f4588357c845845152f4b6700e1d3efeeabd765
Tim Shi <stl501@gmail.com>
2017-06-30 09:46:04 -0700

    Minor changes

--- master-server/main.go
+++ master-server/main.go
@@ -1,13 +1,22 @@
 package main
 
-import "fmt"
 import (
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"fmt"
+	//"github.com/jinzhu/gorm"
+	//_ "github.com/jinzhu/gorm/dialects/postgres"
+	"net/http"
 )
 
+//func main() {
+//	db, _ := gorm.Open("postgres", "host= user=gorm dbname=gorm sslmode=disable password=mypassword")
+//	fmt.Print("hi")
+//	defer db.Close()
+//}
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Print("hello")
+
+}
+
 func main() {
-	db, _ := gorm.Open("postgres", "host=myhost user=gorm dbname=gorm sslmode=disable password=mypassword")
-	fmt.Print("hi")
-	defer db.Close()
+	http.HandleFunc("/", sayHello)
 }
