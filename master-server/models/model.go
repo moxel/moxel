f47653853dd4a5deb6b37fa5207cd435bc587fe0
Tim Shi <stl501@gmail.com>
2017-07-02 01:01:30 -0700

    Add query and delete model in DB

--- master-server/models/model.go
+++ master-server/models/model.go
@@ -25,7 +25,7 @@ type Model struct {
 }
 
 // Compute the unique ID of a Model.
-func GetModelId(userId string, repoId string, tag string) string {
+func ModelId(userId string, repoId string, tag string) string {
 	hash := sha1.New()
 	io.WriteString(hash, userId)
 	io.WriteString(hash, repoId)
@@ -33,11 +33,24 @@ func GetModelId(userId string, repoId string, tag string) string {
 	return base64.URLEncoding.EncodeToString(hash.Sum(nil))
 }
 
-// AddModel inserts a row of type Model into the database.
+// AddModel inserts a row of Model into the database.
 func AddModel(db *gorm.DB, model Model) error {
 	// compute uuid.
-	model.Uid = GetModelId(model.UserId, model.RepoId, model.Tag)
+	model.Uid = ModelId(model.UserId, model.RepoId, model.Tag)
 	// database operations.
 	err := db.Create(&model).Error
 	return err
+}
+
+// GetModelById retrieves the model by Uid
+func GetModelById(db *gorm.DB, modelId string) Model {
+	model := Model{Uid: modelId}
+	db.Where("uid = ?", modelId).First(&model)
+	return model
+}
+
+// DeleteModel removes a row of Model from the database.
+func DeleteModel(db *gorm.DB, modelId string) error {
+	model := Model{Uid: modelId}
+	return db.Delete(&model).Error
 }
