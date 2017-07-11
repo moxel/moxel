a7aed5466b00d48a2720d5d76238ba26e5427370
Ge Yang <yangge1987@gmail.com>
2017-07-11 01:44:48 -0500

    fix express syntax

--- POC/react-server/server/ReactLoader.js
+++ POC/react-server/server/ReactLoader.js
@@ -3,14 +3,13 @@ import {renderToString} from 'react-dom/server'
 
 const Page = (
     <html>
-    <header>
+    <head>
         <script></script>
-    </header>
+    </head>
     <body><h1>this is great!</h1></body>
     </html>
 );
 
 export default function ReactLoader(req, res, next) {
-    console.log(req, res, next);
-    res.send(200, renderToString(Page))
+    res.status(200).send(renderToString(Page))
 }
