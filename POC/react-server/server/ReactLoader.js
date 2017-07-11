d1305602dca44b09699761c51a570d03838327ce
Ge Yang <yangge1987@gmail.com>
2017-07-11 01:37:46 -0500

    got react-server to work with react

--- POC/react-server/server/ReactLoader.js
+++ POC/react-server/server/ReactLoader.js
@@ -1,4 +1,16 @@
+import React from "react";
+import {renderToString} from 'react-dom/server'
+
+const Page = (
+    <html>
+    <header>
+        <script></script>
+    </header>
+    <body><h1>this is great!</h1></body>
+    </html>
+);
+
 export default function ReactLoader(req, res, next) {
     console.log(req, res, next);
-    res.send(200, "<html><header></header><body><h1>this is great!</h1></body></html>")
+    res.send(200, renderToString(Page))
 }
