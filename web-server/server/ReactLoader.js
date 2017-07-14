c2cec66994bb47d158d0f582ed56fae2f539aadc
Ge Yang <yangge1987@gmail.com>
2017-07-13 23:45:01 -0500

    got react-primitive to work.

--- web-server/server/ReactLoader.js
+++ web-server/server/ReactLoader.js
@@ -5,6 +5,7 @@ import {StaticRouter} from "react-router-dom";
 import {ServerStyleSheet, StyleSheetManager} from 'styled-components'
 import Root from "../src/Root";
 import fs from 'fs';
+import {StyleSheet} from 'react-primitives';
 
 const HTML = fs.readFileSync(__dirname + '/../public/index.html').toString();
 
@@ -12,16 +13,25 @@ const HTML = fs.readFileSync(__dirname + '/../public/index.html').toString();
 export default function ReactLoader(req, res, next) {
     const location = req.url.toString();
 
-    const sheet = new ServerStyleSheet();
-    const html = renderToString(sheet.collectStyles(
+    // const sheet = new ServerStyleSheet();
+    // const html = renderToString(sheet.collectStyles(
+    //     <StaticRouter location={location} context={{}}>
+    //         <Root/>
+    //     </StaticRouter>
+    // ));
+    // const css = sheet.getStyleTags();
+    const html = renderToString(
         <StaticRouter location={location} context={{}}>
             <Root/>
         </StaticRouter>
-    ));
-    const css = sheet.getStyleTags();
+    );
+    const css = StyleSheet
+        .getStyleSheets()
+        .map(({id, textContent}) => `<style id=${id}>${textContent}</style>`)
+        .join('');
     res.status(200).send(
         HTML
-            .replace(/<!-- SSR:HTML -->/, html)
             .replace(/<!-- SSR:CSS -->/, css)
+            .replace(/<!-- SSR:HTML -->/, html)
     );
 }
--- web-server/src/Root.js
+++ web-server/src/Root.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import {Route} from "react-router-dom";
+import {View, Text, StyleSheet} from 'react-primitives';
 // import SearchViews from './pages/search-view';
 // import ModelViews from './pages/model-view';
 // import DatasetViews from './pages/dataset-view';
@@ -13,18 +14,31 @@ const StyledDiv = styled.div`
     position: relative;
     background-color: red;
 `;
+
+const TestStyle = StyleSheet.create({
+    foo: {
+        color: "red",
+        backgroundColor: 'green'
+    }
+});
 export default function Root() {
     return (
-        <div>
-            <Route exact path="/" component={() => <div>root page</div>}/>
-            <Route exact path="/test-1" component={() => <div>test-1 page</div>}/>
-        </div>
+        <View>
+            <Route exact path="/" component={() => <Text style={TestStyle.foo}>root page</Text>}/>
+            <Route exact path="/test-1" component={() => <Text>test-1 page</Text>}/>
+        </View>
         // <StyledDiv style={{color: "green"}}>test this 3</StyledDiv>
     );
 }
-{/*<PageLayout>*/}
-{/*<Route exact path="/" component={() => <div>root page</div>}/>*/}
-{/*{SearchViews}*/}
-{/*{ModelViews}*/}
-{/*{DatasetViews}*/}
-{/*</PageLayout>*/}
+{/*<PageLayout>*/
+}
+{/*<Route exact path="/" component={() => <div>root page</div>}/>*/
+}
+{/*{SearchViews}*/
+}
+{/*{ModelViews}*/
+}
+{/*{DatasetViews}*/
+}
+{/*</PageLayout>*/
+}
