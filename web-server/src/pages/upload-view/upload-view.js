a207c5bf6b306ce3fa30e7bda1f1c909e1034efc
Tim Shi <stl501@gmail.com>
2017-07-21 12:56:14 -0700

    Add stepper with warpdrive installation instruction

--- web-server/src/pages/upload-view/upload-view.js
+++ web-server/src/pages/upload-view/upload-view.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import PropTypes from "prop-types";
 import FixedWidthRow from "../../components/fixed-width-row";
 import ModelSnippet from "../../components/model-snippet/model-snippet";
@@ -9,51 +9,54 @@ import TabButtonBar from "../../components/tab-button-bar";
 import 'markdown-it';
 import Markdown from 'react-markdownit';
 import styled from "styled-components";
+import Stepper from 'react-stepper-horizontal';
+
+class UploadView extends Component {
+    constructor() {
+        super()
+        this.state = {
+            step: 0
+        }
+
+        this.nextStep = this.nextStep.bind(this);
+    }
+
+    nextStep() {
+        console.log('changing state', this.state.step)
+        this.setState({
+            step: this.state.step + 1
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <Stepper steps={ [{title: 'Install Warpdrive'}, {title: 'Deploy Models'}, {title: 'Finish'}] } activeStep={ this.state.step } />
+                <br/>
+                <FixedWidthRow>
+                    <Markdown tagName="instruction">
+                    {`    
+                        ##### Warpdrive
+
+                        Install this CLI tool to deploy models.
+
+                        \`\`\`
+                        pip install warp-cli
+                        \`\`\`
 
 
-function UploadView() {
-    return (
-        <ul className="stepper horizontal">
-           <li className="step active">
-              <div className="step-title waves-effect">E-mail</div>
-              <div className="step-content">
-                 <div className="row">
-                    <div className="input-field col s12">
-                       <input id="email" name="email" type="email" className="validate" required/>
-                       <label htmlFor="first_name">Your e-mail</label>
-                    </div>
-                 </div>
-                 <div className="step-actions">
-                    <button className="waves-effect waves-dark btn next-step">CONTINUE</button>
-                 </div>
-              </div>
-           </li>
-           <li className="step">
-              <div className="step-title waves-effect">Passo 2</div>
-              <div className="step-content">
-                 <div className="row">
-                    <div className="input-field col s12">
-                       <input id="password" name="password" type="password" className="validate" required/>
-                       <label htmlFor="password">Your password</label>
-                    </div>
-                 </div>
-                 <div className="step-actions">
-                    <button className="waves-effect waves-dark btn next-step">CONTINUE</button>
-                    <button className="waves-effect waves-dark btn-flat previous-step">BACK</button>
-                 </div>
-              </div>
-           </li>
-           <li className="step">
-              <div className="step-title waves-effect">Fim!</div>
-              <div className="step-content">
-                 Finish!
-                 <div className="step-actions">
-                    <button className="waves-effect waves-dark btn" type="submit">SUBMIT</button>
-                 </div>
-              </div>
-           </li>
-        </ul>
-    );
+                    `}
+                    </Markdown>
+
+                
+                </FixedWidthRow>
+
+                <FixedWidthRow>
+                    <button style={{float: "right"}} className="waves-effect waves-light btn blue" onClick={this.nextStep}>Next</button> 
+                </FixedWidthRow>
+            </div>
+        );
+    }
 }
 UploadView.propTypes = {
     
