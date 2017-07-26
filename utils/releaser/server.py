0ce1f8b153d3c81d6bfaa48ccfe533ed2d4022e2
Tim Shi <stl501@gmail.com>
2017-07-25 23:21:09 -0700

    Use redirect and segment analytics

--- utils/releaser/server.py
+++ utils/releaser/server.py
@@ -1,13 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, request, redirect
+import analytics
+from datetime import datetime
+
+analytics.write_key = "Mezspk1rncQoF44Wxi2hy6rmsdDPWfEB"
 
 BUCKET = 'dummy-release'
 
 app = Flask(__name__)
 
+
 @app.route('/<path:path>', methods=['GET'])
 def index(path):
-    return render_template('index.html', path=path)
+    user = request.args.get('user', 'unknown')
+    analytics.track(user, 'Download CLI', {
+        'datetime': datetime.now().isoformat()
+    })
+    return redirect('https://storage.googleapis.com/dummy-release/{}'.format(path))
 
 
-app.run(debug=False, port=8080, host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(debug=False, port=8080, host='0.0.0.0')
 
