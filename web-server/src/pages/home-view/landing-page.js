f300d4abe9a9c2987dfeda11853c1faaa018fe9e
Tim Shi <stl501@gmail.com>
2017-07-28 16:48:07 -0700

    Update front-end style

--- web-server/src/pages/home-view/landing-page.js
+++ web-server/src/pages/home-view/landing-page.js
@@ -7,13 +7,28 @@ import Markdown from 'react-markdownit';
 import { Button, Card, Row, Col } from 'react-materialize';
 
 class LandingPage extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      dogURL: "/images/tf-dog.jpg"
+    }
+  }
+
+  detectDog() {
+    this.setState({
+      dogURL: "/images/tf-dog-box.png"  
+    })
+  }
+
   render() {
+
     return (
       <div>
     
         <div className="row" style={{paddingTop: "100px", paddingBottom: "0px", marginBottom: "0px"}}>
             <div className="col s4 m5 offset-m1">
-              <div style={{height: "180px"}}>
+              <div style={{height: "100px"}}>
               </div>
               <div style={{fontSize: "40px", textAlign: "center"}}>
                 Deploy Your <br/> Machine Learning Model
@@ -36,72 +51,75 @@ class LandingPage extends Component {
         </div>
         <div className="row" style={{height: "700px", backgroundColor: "#f6f9fc", marginBottom: "0px"}}>
           <div className="col s8 offset-s2 m10 offset-m1" >
-            <div style={{height: "80px"}}>
+            <div style={{height: "100px"}}>
             </div>
             <div style={{fontSize: "40px", textAlign: "center"}}>
-              Discover the best <b>open</b> AI models 
+              Discover the Best Open AI models 
             </div>
             <div style={{fontSize: "25px", textAlign: "center"}}>
-              Browse through trending models. Access models through REST APIs.
+              Browse through trending models. Access models through APIs.
             </div>
             <div style={{height: "80px"}}>
             </div>
 
             <div className="row">
-              <div className="col s4 m5" style={{boxShadow: "0px 0px 100px #aaa", height: "300px", backgroundColor: "rgb(246, 248, 250)", padding: "0px"}}>
-                <Markdown tagName="instruction" className="markdown-body ">
-                  <br/>
-                  {`   
-                      \`\`\`python
-                      import requests, os, base64
-                      URL = "http://beta.dummy.ai"
+              <div className="col s10 m12">
+                <div className="col s4 m5" style={{boxShadow: "0px 0px 100px #aaa", height: "300px", backgroundColor: "rgb(246, 248, 250)", padding: "0px", position: "relative"}}>
+                  <Markdown tagName="instruction" className="markdown-body " style={{overflow: "hidden"}}>
+                    <br/>
+                    {`   
+                        \`\`\`python
+                        import requests, os, base64
+                        URL = "http://beta.dummy.ai"
 
-                      response = requests.post(
-                        URL + "/dummy/tf-object-detection/latest", 
-                        json={'image': image}
-                      ).json()
+                        # Send a POST request to API endpoint
+                        response = requests.post(
+                          URL + "/dummy/tf-object-detection/latest", 
+                          json={'image': image}
+                        ).json()
 
-                      \`\`\`
+                        \`\`\`
 
-                  `}
-                </Markdown>  
-              </div>
-              <div className="col s4 m5 offset-m2" style={{boxShadow: "0px 0px 100px #aaa", height: "300px", overflow: "hidden", padding: "0px"}}>
-                <img src="/images/tf-dog.jpg" style={{width: "auto", height: "100%"}}></img>
-              </div>
-            </div>
-
-
-            <div className="row">
-              <div style={{height: "20px"}}>
-              </div>
-              <div style={{fontSize: "15px", textAlign: "center"}}>
-                <Button waves="light" className="green">Run</Button>
+                    `}
+                  </Markdown>  
+                  <Button id="run-button" name="run-button" waves="light" className="blue" style={{float: "right", right: "5px", bottom: "5px", position: "absolute"}} onClick={() => this.detectDog()}>Run</Button>
+                </div>
+                <div className="col s4 m5 offset-m2" style={{boxShadow: "0px 0px 100px #aaa", height: "300px", overflow: "hidden", padding: "0px", backgroundImage: `url(${this.state.dogURL})`, backgroundSize: "cover"}}>
+                </div>
               </div>
             </div>
 
           </div>
         </div>
 
+        <div className="row" style={{backgroundColor: "rgb(252, 253, 253)", marginBottom: "0px"}}>
+          <div className="col s12 ">
+            <div style={{height: "100px"}}> 
+            </div>
+          </div>
+        </div>
 
-        <div className="row" style={{paddingTop: "100px", height: "700px", paddingBottom: "0px", marginBottom: "0px", backgroundImage: "url(\"/images/dock.png\")", backgroundSize: "100% auto"}}>
+        <div className="row" style={{paddingTop: "100px", height: "700px", paddingBottom: "0px", marginBottom: "0px", backgroundImage: "url(\"/images/dock.png\")", backgroundSize: "cover"}}>
+
+
+            <div className="col s12 ">
+              <div className="col s4 m5 ">
+                <div style={{width: "650px", overflow:"hidden"}}> 
+                </div>
+              </div>
+
+              <div className="col s4 m5 ">
+                <div style={{fontSize: "40px", textAlign: "center"}}>
+                  Focus on the Great Work
+                </div>
+                <div style={{fontSize: "25px", textAlign: "center"}}>
+                  We serve it to the world for you.
+                </div>
+
+              </div>  
+            </div>
+
             
-            <div className="col s4 m5 offset-m1 ">
-              <div style={{width: "650px", overflow:"hidden"}}> 
-              </div>
-            </div>
-
-            <div className="col s4 m5 ">
-              <div style={{height: "60px"}}>
-              </div>
-              <div style={{fontSize: "40px", textAlign: "center"}}>
-                You Focus on the Great Work
-              </div>
-              <div style={{fontSize: "30px", textAlign: "center"}}>
-                We serve it to the world for you.
-              </div>
-
-            </div>
         </div>
       </div>
     );
--- web-server/src/pages/layout/page-header.js
+++ web-server/src/pages/layout/page-header.js
@@ -42,39 +42,37 @@ class PageHeader extends Component {
             banner = (
                 <div className="nav-header center">
                     <br/>
-                    <h1 style={{fontSize: "70px"}}>World's Best Models <br/> Built by the Community</h1>
-                    <div className="tagline" style={{lineHeight: 6}}>
+                    <h1 style={{fontSize: "65px", fontWeight: 400}}>World's Best Models <br/> Built by the Community</h1>
+                    <div className="tagline" style={{lineHeight: 6, fontSize: 20}}>
                         Dummy.ai is a platform to build and share machine intelligence.
-                        <br/>
-                        <div>
-                            <Button waves="light" className="blue" onClick={() => AuthStore.login('/new')}>Upload Model</Button> 
-                            &nbsp;&nbsp;&nbsp;&nbsp; 
-                            <Button waves="light" className="green" onClick={() => window.location.href = "/models"}>Discover Model</Button>
-                            &nbsp; 
-                        </div>
-                        <br/>
                     </div>
+                     <div className="row">
+                        <form className="col s10 offset-s1" action="//xyz.us1.list-manage.com/subscribe/post?u=dfe1971430cfa22545ae7ba85&amp;id=5fc1f57ce4" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
+                            <div id="mc_embed_signup">
+                                <div id="mc_embed_signup_scroll">
+                                    <div className="row">
+                                        <div className="col s6 offset-s2">
+                                            <input id="email" style={{border: "none", borderRadius: "5px", width: "100%", height: "40px", backgroundColor: "white", color: "black"}} type="email" name="EMAIL" className="email validate" placeholder="Email Adress"/>
+                                        </div>
+                                        <div className="col s2" style={{lineHeight: "0px", marginLeft: "20px"}}>
+                                            <input type="submit" value="Sign Up" name="subscribe" id="mc-embedded-subscribe" className="btn btn-wavs green" style={{paddingLeft: "0px", paddingRight: "0px", lineHeight: "0px", margin: "0px", height: "40px"}}/>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <br/>
                 </div>
             )
         }
-        let menu = null;
-        if(!AuthStore.isAuthenticated()) {
-            menu = (
-                <ul className="right hide-on-med-and-down">
-                  <li className="active"><a href="/models">Models</a></li>
-                  <li><a href="Warpdrive.html">Warpdrive</a></li>
-                  <li><a href="blog.html">Blog</a></li>
-                  <li><a href="http://docs.dummy.ai/">Docs</a></li>
-                  <li><a className='dropdown-button' href='#' data-activates='feature-dropdown' data-belowOrigin="true" data-constrainWidth="false">About<i className="material-icons right">arrow_drop_down</i></a></li>
-                </ul>
-            )
-        }else{
-            menu = (
-                <ul className="right hide-on-med-and-down">
-                  <li><a href="/logout">Logout</a></li>
-                </ul>
-            )
-        }
+        var menu = (
+            <ul className="right hide-on-med-and-down">
+              <li className="active"><a href="/">Models</a></li>
+              {/*<li><a href="warpdrive.html">Warpdrive</a></li>
+               <li><a href="http://docs.dummy.ai/">Docs</a></li>*/}
+            </ul>
+        )
         return (
             <div>
                 <Mask show={this.state.isOpen}></Mask>
@@ -82,7 +80,7 @@ class PageHeader extends Component {
                   onClose={this.toggleModal} useAuth0="true">
                 </SignupModal>
 
-                <nav className="nav-extended">
+                <nav className="nav-extended" style={{boxShadow: "none"}}>
                     <div className="nav-background">
                         <div className="pattern active" style={{backgroundImage: "url('http://cdn.shopify.com/s/files/1/1775/8583/t/1/assets/icon-seamless.png')"}}></div>
                     </div>
@@ -93,6 +91,8 @@ class PageHeader extends Component {
                         {banner}
                     </div>
                 </nav>
+
+               
             </div>
 
            
