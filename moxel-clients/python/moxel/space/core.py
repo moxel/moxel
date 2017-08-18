e4340f31fc87cf3482f377dadbfce3a0cdff1291
Tim Shi <stl501@gmail.com>
2017-08-18 02:14:05 -0700

    Add moxel python client

---
+++ moxel-clients/python/moxel/space/core.py
@@ -0,0 +1,7 @@
+class Space(object):
+    """ The interface for data semantic space.
+    """
+    def contains(self, x):
+        """ Check if point x is inside Space.
+        """
+        return True
