3ff9f13fbd5d9eb4df74558c9726cb61d224404f
LinxiFan <jimfanspire@gmail.com>
2017-09-11 00:48:25 -0700

    Enable pip install

--- moxel-clients/python/setup.py
+++ moxel-clients/python/setup.py
@@ -1,13 +1,34 @@
+from __future__ import print_function
 import os
-from setuptools import setup
+import sys
+import glob
+from setuptools import setup, find_packages
+
+# PATH = os.path.join(os.path.dirname(__file__))
 
 def read(fname):
-    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+    with open(fname) as f:
         return f.read().strip()
+
+def get_files(folder):
+    return [f for f in glob.iglob(os.path.join(folder, '**'), recursive=True) 
+            if not os.path.isdir(f)]
+
+# print(get_files('bin'))
+
+if 0:
+    _p = sys.platform
+    if _p.startswith('linux'):
+        bin_path = 'linux'
+    elif _p == 'darwin':
+        bin_path = 'osx'
+    elif _p == 'win32':
+        bin_path = 'windows'
+    bin_path = os.path.join('bin', bin_path, 'moxel')
 
 
 setup(name='moxel',
-      version='0.0.1',
+      version='0.0.0-0',
       author='Moxel team',
       author_email='support@moxel.ai',
       url='http://moxel.ai',
@@ -15,10 +36,8 @@ setup(name='moxel',
       long_description=read('README.rst'),
       keywords=['Machine Learning', 'Model', 'Deep Learning', 'Platform'],
       license='GPLv3',
-      packages=['moxel'],
-      entry_points={
-        'console_scripts': []
-      },
+      # https://stackoverflow.com/questions/29609141/why-doesnt-setuptools-copy-modules-in-a-subfolder
+      packages=find_packages('.'),
       classifiers=[
           "Development Status :: 2 - Pre-Alpha",
           "Topic :: Scientific/Engineering :: Artificial Intelligence",
@@ -27,5 +46,14 @@ setup(name='moxel',
       ],
       install_requires=read('requirements.txt').splitlines(),
       include_package_data=True,
+      # https://stackoverflow.com/questions/7522250/how-to-include-package-data-with-setuptools-distribute
+      # package_data={'moxel': [bin_path]},
+      # scripts=['bin/moxel'], # script has to be python file
+      entry_points = {
+          'console_scripts': ['moxel=moxel.command_line:main'],
+      },
+      data_files=[('bin/osx', ['bin/osx/moxel']), 
+                 ('bin/linux', ['bin/linux/moxel']),
+                 ('bin/windows', ['bin/windows/moxel'])],
       zip_safe=False
 )
