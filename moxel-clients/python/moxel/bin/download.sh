4c40fa2c2268ddebd1ece682c3ab7501599b3e91
Tim Shi <stl501@gmail.com>
2017-09-18 11:21:03 -0700

    Remove binaries from Git! They are HUGE.

--- moxel-clients/python/moxel/bin/download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-URL='http://beta.moxel.ai/release/cli/latest'
-
-for platform in osx linux windows
-do
-    mkdir -p $platform
-    wget -O $platform/moxel $URL/$platform/moxel
-    chmod +x $platform/moxel
-done
