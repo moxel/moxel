2a01b05f661f2fe29e4ffc73103aa349034ef6cd
Tim Shi <stl501@gmail.com>
2017-10-03 17:26:28 -0700

    Bump moxel versions

--- moxel-clients/python/setup.py
+++ moxel-clients/python/setup.py
@@ -27,7 +27,7 @@ for name in ['linux', 'osx', 'windows']:
     bin_paths.append(os.path.join('moxel', 'bin', name, 'moxel'))
 
 setup(name='moxel',
-      version='0.0.3.post29',
+      version='0.0.3.post30',
       author='Moxel team',
       author_email='support@moxel.ai',
       url='http://moxel.ai',
