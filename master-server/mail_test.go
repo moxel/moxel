e88c4446d46fc42b91ea35133218b20007bec904
Tim Shi <stl501@gmail.com>
2017-10-05 12:16:04 -0700

    Send out second mail

--- master-server/mail_test.go
+++ master-server/mail_test.go
@@ -73,3 +73,39 @@ func TestSendFirstNewsLetter(t *testing.T) {
 	}
 
 }
+
+func TestSendSecondNewsLetter(t *testing.T) {
+	accessToken, err := GetAuth0AccessToken()
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+
+	users, err := GetAuth0Users(accessToken)
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+
+	for _, user := range users {
+		fmt.Println("user", user["nickname"], user["email"])
+		templateData, _ := ioutil.ReadFile("templates/new-model.html")
+		template := string(templateData)
+		data := map[string]string{
+			"user":        user["nickname"].(string),
+			"author":      "moxel",
+			"model":       "neural-storyteller",
+			"title":       "Neural Networks Tell Little Stories about Images",
+			"description": `neural-storyteller is a recurrent neural network that generates little stories about images. This repository contains code for generating stories with your own images, as well as instructions for training new models. The model might take ~1min to generate the story.`,
+			"link":        "http://beta.moxel.ai/models/moxel/neural-storyteller/latest",
+			"recipient":   user["email"].(string),
+		}
+
+		err := SendTemplateEmail("A New Model Neural Storyteller is Now Live!", template, []map[string]string{data})
+		if err != nil {
+			t.Errorf(err.Error())
+			return
+		}
+	}
+
+}
